ecd727f71a0ce34adab4017668ccb8396f9a5bdb
farhandesign <farhan.design@outlook.com>
2021-05-18 18:37:05 +0400

    a

--- src/context/MovieState.js
+++ src/context/MovieState.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import AuthContext from './auth-context';
 
 const AuthState = (props) => {
-	const [ user, setUser ] = useState(null);
-	const value = useMemo(() => ({ user, setUser }), [ user, setUser ]);
-
-	return <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>;
+	const [ query, setQuery ] = useState("");
+	
+	return <AuthContext.Provider value={query}>{props.children}</AuthContext.Provider>;
 };
 
 export default AuthState;
